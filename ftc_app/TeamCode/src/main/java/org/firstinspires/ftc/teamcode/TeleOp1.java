55950b87a0cdbca8a509074d77fafce9d8627d9e
KadenGordon <kaden.m.gordon@gmail.com>
2017-07-13 19:40:38 -0600

    initial commit, has first teleop program

---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp1.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "DemoTeleOp", group = "linear OpMode")
+public class TeleOp1 extends OpMode {
+
+    DcMotor Left;
+    DcMotor Right;
+
+    @Override
+
+
+    public void init() {
+        Left = hardwareMap.dcMotor.get("m1");
+        Right = hardwareMap.dcMotor.get("m2");
+
+        reverseMotor(Left);
+
+    }
+
+    @Override
+    public void loop() {
+        /*
+        float right = gamepad1.right_stick_y;
+        float left = gamepad1.left_stick_y;
+        */
+        Right.setPower(gamepad1.right_stick_y);
+        Left.setPower(gamepad1.left_stick_y);
+    }
+
+    public void reverseMotor(DcMotor motor) {
+        motor.setDirection(DcMotor.Direction.REVERSE);
+    }
+}
