057f75656da68019e2e047dc2bc50bec7f64e5f7
KadenGordon <kaden.m.gordon@gmail.com>
2017-07-20 18:18:19 -0600

    added a few teleop files

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp1.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp1.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name = "DemoTeleOp", group = "linear OpMode")
+@TeleOp(name = "2MotorTeleOp", group = "linear OpMode")
 public class TeleOp1 extends OpMode {
 
     DcMotor Left;
---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp4Motor.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "4MotorTeleOp", group = "linear OpMode")
+public class TeleOp4Motor extends OpMode {
+
+    DcMotor FrontLeft;
+    DcMotor FrontRight;
+    DcMotor RearLeft;
+    DcMotor RearRight;
+
+    @Override
+
+
+    public void init() {
+        FrontLeft = hardwareMap.dcMotor.get("m1");
+        FrontRight = hardwareMap.dcMotor.get("m2");
+        RearLeft = hardwareMap.dcMotor.get("m3");
+        RearRight = hardwareMap.dcMotor.get("m4");
+
+        reverseMotor(FrontLeft);
+        reverseMotor(RearLeft);
+
+
+    }
+
+    @Override
+    public void loop() {
+
+        double right = gamepad1.right_stick_y;
+        double left = gamepad1.left_stick_y;
+
+        FrontRight.setPower(right);
+        FrontLeft.setPower(left);
+        RearRight.setPower(right);
+        RearLeft.setPower(left);
+
+
+        telemetry.addData("Left power: ", left);
+        telemetry.addData("Right power: ", right);
+    }
+
+    public void reverseMotor(DcMotor motor) {
+
+        motor.setDirection(DcMotor.Direction.REVERSE);
+    }
+}
