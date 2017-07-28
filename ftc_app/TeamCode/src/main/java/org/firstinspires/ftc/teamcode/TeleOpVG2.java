9fb6bfb02a1545bf64c600c7085caac45fa1b5ab
KadenGordon <kaden.m.gordon@gmail.com>
2017-07-28 12:35:35 -0600

    Kaden added a TeleOp Austin wanted. Not tested

---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpVG2.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import java.lang.*;
+
+@TeleOp(name = "TeleOpVG", group = "linear OpMode")
+public class TeleOpVG extends OpMode {
+
+    DcMotor Left;
+    DcMotor Left2;
+    DcMotor Right;
+    DcMotor Right2;
+
+    @Override
+
+
+    public void init() {
+        Left = hardwareMap.dcMotor.get("m1");
+        Right = hardwareMap.dcMotor.get("m2");
+        Left2 = hardwareMap.dcMotor.get("m3");
+        Right2 = hardwareMap.dcMotor.get("m4");
+
+        reverseMotor(Left);
+        reverseMotor(Left2);
+    }
+
+    @Override
+    public void loop() {
+        float speed = gamepad1.right_trigger;
+        float steering = gamepad1.left_stick_x;
+        float middle = speed - Math.abs(steering);
+
+        if (steering > 0) {
+            float right = middle - steering;
+            float left = middle + steering;
+        }
+        else if (steering < 0) {
+            float right = middle + steering;
+            float left = middle - steering;
+        }
+
+        else {
+            float right = speed;
+            float left = speed;
+        }
+
+        Right.setPower(right);
+        Right2.setPower(right);
+        Left.setPower(left);
+        Left2.setPower(left);
+    }
+
+    public void reverseMotor(DcMotor motor) {
+
+        motor.setDirection(DcMotor.Direction.REVERSE);
+    }
+}
