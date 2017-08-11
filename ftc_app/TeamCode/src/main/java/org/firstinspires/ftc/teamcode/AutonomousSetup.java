39e3befa956358b5bf08e237a6309028e0e63ad1
DarkMatterKid <lwpop@icloud.com>
2017-08-10 20:09:43 -0600

    Elaborated the autonomous program

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousSetup.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousSetup.java
@@ -4,7 +4,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+import android.util.Log;
 //This makes the OpMode available in the Autonomous group under the name 'Autonomous', in the Driver Station
 @Autonomous(name = "Autonomous", group = "Autonomous")
 
@@ -15,7 +15,10 @@ public class AutonomousSetup extends LinearOpMode {
 
     // Now declare any universal value you will need more then once, like encoder CPR(Clicks per rotation)
     int CPR = 1120; //Rough estimate of the encoders CPR
-
+    int Tm = 16; // Number of teeth in the gear attatched to the motor
+    int Tw = 16; //Number of teeth in the gear attatched to the wheels
+    double D = 2.5; //Diameter of wheels
+    double C = D * Math.PI;//One rotation of tank gear/wheel
     public void runOpMode() throws InterruptedException {
         //Start with the basic declaration of variable strings that the phones will read
         Motor = hardwareMap.dcMotor.get("m1");
@@ -30,19 +33,58 @@ public class AutonomousSetup extends LinearOpMode {
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         //The Best Way to run autonomous is to make several functions outside this box, and just run those functions here, with different values to change how far to go or for how long
-        stopMotor();
-
-
-
-
-
-
+        inches(1, 0);
 
 
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     }
     //Everything Past this point should ONLY be functions. It is outside the run function, so it can only be called as a function.
-    void stopMotor(){
+
+    void inches(double ForwardInches, double power) {
+        boolean isNegative = false;
+        if (ForwardInches < 0) {
+            isNegative = true;
+        }
+        double start, now, goal;
+        int distancePerClicks = (int) (((Tw / Tm) * (ForwardInches * CPR) / C)) / 2;
+
+
+        telemetry.addData("inches", "inches  " + ForwardInches);
+        telemetry.addData("inches", "inches  " + distancePerClicks);
+        start = encodervalue();
+        now = start;
+        goal = start + distancePerClicks;
+        if (isNegative == true) {
+            forward(-power);
+        } else {
+            forward(power);
+        }
+        telemetry.update();
+        while (now < goal) {
+
+
+            now = encodervalue();
+        }
+
+
+        stopmoving();
+
+    }
+
+    void forward(double power) {
+        Motor.setPower(power);
+    }
+
+    void stopmoving() {
         Motor.setPower(0.0);
+    }
+
+    int encodervalue() {
+        int m1;
+        m1 = Motor.getCurrentPosition();
+        telemetry.addData("Encoder", m1);
+        telemetry.update();
+        return (m1) / 1;
+
     }
 }
