27374c7e7911fe85142ecea01401b977897ef71c
“cooler9711” <cooler9711@outlook.com>
2017-08-13 16:37:35 +0000

    “changed-up-the-fs”

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpVG2.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import java.lang.*;
+
+
+//should be the one that controls like a racing gRearLeft@TeleOp(name = "TeleOpVG", group = "linear OpMode")
+public class TeleOpVG2 extends OpMode {
+
+    DcMotor FrontLeft;
+    DcMotor RearLeft;
+    DcMotor FrontRight;
+    DcMotor RearRight;
+
+    @Override
+
+
+    public void init() {
+        FrontLeft = hardwareMap.dcMotor.get("m1");
+        FrontRight = hardwareMap.dcMotor.get("m2");
+        RearLeft = hardwareMap.dcMotor.get("m3");
+        RearRight = hardwareMap.dcMotor.get("m4");
+        reverseMotor(FrontLeft);
+        reverseMotor(RearLeft);
+    }
+
+    @Override
+    public void loop() {
+        float speed = gamepad1.right_trigger;
+        float steering = gamepad1.left_stick_x;
+        float middle = speed - Math.abs(steering);
+        float right = 0;
+        float left = 0;
+
+        if (steering > 0) {
+            right = middle - steering;
+            left = middle + steering;
+        }
+        else if (steering < 0) {
+            right = middle + steering;
+            left = middle - steering;
+        }
+
+        else {
+            right = speed;
+            left = speed;
+        }
+
+        FrontRight.setPower(right);
+        RearRight.setPower(right);
+        FrontLeft.setPower(left);
+        RearLeft.setPower(left);
+    }
+
+    public void reverseMotor(DcMotor motor) {
+
+        motor.setDirection(DcMotor.Direction.REVERSE);
+    }
+// TODO: Test on robots
+}
