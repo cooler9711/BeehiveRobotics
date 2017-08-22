e812106b54c74850b77bbf2300a7a174c902f6ec
Peter Beall <peterbeall84@gmail.com>
2017-08-22 15:12:57 -0600

    added my DPad program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDPad.java
@@ -0,0 +1,85 @@
+//This program lets the robot be controlled by the dpad.
+//You can also change the speed of the robot with the a, b, y, and x buttons.
+//a is fastest, b is fast, x is slow, y is slowest.
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "TeleOpDPad ", group = "linear OpMode")
+public class TeleOpDPad extends OpMode {
+
+    DcMotor FrontRight;
+    DcMotor BackRight;
+    DcMotor FrontLeft;
+    DcMotor BackLeft;
+
+    double Speed = 1;
+
+    public void init() {
+
+        FrontLeft = hardwareMap.dcMotor.get("m1");
+        FrontRight = hardwareMap.dcMotor.get("m2");
+        BackLeft = hardwareMap.dcMotor.get("m3");
+        BackRight = hardwareMap.dcMotor.get("m4");
+
+        FrontRight.setDirection(DcMotor.Direction.REVERSE);
+        BackRight.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    public void loop() {
+
+        //Changing the speed variable if needed:
+        if (gamepad1.a) {
+            Speed = 1;
+        }
+        else if (gamepad1.b) {
+            Speed = 0.6;
+        }
+        else if (gamepad1.x) {
+            Speed = 0.3;
+        }
+        else if (gamepad1.y) {
+            Speed = 0.1;
+        }
+
+        //Setting the motors' speed:
+        if (gamepad1.dpad_up) {
+
+            FrontLeft.setPower(Speed);
+            FrontRight.setPower(Speed);
+            BackLeft.setPower(Speed);
+            BackRight.setPower(Speed);
+        }
+        else if (gamepad1.dpad_right) {
+
+            FrontLeft.setPower(Speed);
+            FrontRight.setPower(-Speed);
+            BackLeft.setPower(Speed);
+            BackRight.setPower(-Speed);
+        }
+        else if (gamepad1.dpad_left) {
+
+            FrontLeft.setPower(-Speed);
+            FrontRight.setPower(Speed);
+            BackLeft.setPower(-Speed);
+            BackRight.setPower(Speed);
+        }
+        else if (gamepad1.dpad_down) {
+
+            FrontLeft.setPower(-Speed);
+            FrontRight.setPower(-Speed);
+            BackLeft.setPower(-Speed);
+            BackRight.setPower(-Speed);
+        }
+        else {
+            FrontLeft.setPower(0);
+            FrontRight.setPower(0);
+            BackLeft.setPower(0);
+            BackRight.setPower(0);
+        }
+
+    }
+}
