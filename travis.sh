677088ae43b5c6b0f801dfe4fc963fd163184375
cooler9711 <cooler9711@users.noreply.github.com>
2017-09-08 08:22:33 -0600

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-NOW=$(date +"%Y-%m-%d")
-echo "$NOW"
-# put sftp stuff here
-echo "mkdir $NOW"
-ls
+mkdir TeamCode/build/$NOW
+ls TeamCode/build
+cp -r TeamCode/build/outputs/apk TeamCode/build/$NOW
+echo $piPass | sftp root@24.2.66.247 -o stricthostkeychecking=no
+cd /var/www/robotics
+mkdir $NOW
+cd $NOW
+put TeamCode/build/$NOW
 
 # Using backticks you can put it in any command
